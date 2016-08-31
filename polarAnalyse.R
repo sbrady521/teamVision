deae1ab722c86f5339b23a16c236d6a9eda9a2c0
garyyb <garyybai@gmail.com>
2016-08-31 23:58:58 +1000

    Starting some work on Hough Transformations...a different way of fitting lines (albeit similar to linear regression)

---
+++ polarAnalyse.R
@@ -0,0 +1,10 @@
+polartable <- read.table("polar", header = FALSE)
+
+#print(polartable)
+
+png(filename = "polarplot.png", width = 1000, height = 500)
+
+smoothScatter(polartable[,2], polartable[,1], 
+        main = "Hough Transformation Data", xlab = "Theta", ylab = "r")
+
+dev.off()
